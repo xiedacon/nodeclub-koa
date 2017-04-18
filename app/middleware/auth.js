4d591d969f05f3b6f2e3b1ba7fe809b7107a40a9
xiedacon <xiedacon@qq.com>
2017-04-18 22:03:13 +0800

    update middleware auth

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,6 +1,45 @@
+'use strict'
+const config = require('config-lite');
+const cookie = config.cookie;
+const adminNames = config.admin.names;
+const Message = require('../service').Message;
+const User = require('../service').User;
+const separator = '$$$$';
+
 module.exports = {
-  adminRequired
-  userRequired
-  blockUser
-  authUser
+  adminRequired: () => {},
+  userRequired: () => {},
+  blockUser: (ctx, next) => {
+    if (ctx.path !== '/signout' && ctx.session.user && ctx.session.user.is_block && ctx.method !== 'GET') {
+      ctx.status = 403;
+      ctx.body = '您已被管理员屏蔽了。有疑问请联系 @alsotang。';
+      return;
+    }
+    
+    return next();
+  },
+  authUser: async (ctx, next) => {
+    let user = ctx.session.user,
+      auth_token = ctx.cookies.get(cookie.name, {signed: true});
+      
+    if (!user && auth_token) {
+      let user_id = auth_token.split(separator)[0];
+      user = await User.getById(user_id);
+    }
+
+    if (user) {
+      if (adminNames.indexOf(user.loginname) >= 0) {
+        user.is_admin = true;
+      }
+
+      user.messages_count = await Message.getCountById(user._id);
+      ctx.state.current_user = user;
+    }
+    
+    return next();
+  },
+  gen_session: (user, ctx) => {
+    let auth_token = user._id + separator; // 以后可能会存储更多信息，用 $$$$ 来分隔
+    ctx.cookies.set(cookie.name, auth_token, cookie); //cookie 有效期30天
+  }
 };
