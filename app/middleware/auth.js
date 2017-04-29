ec75700dbf2294fa4f92b54418c013f108a5a44a
xiedacon <xiedacon@qq.com>
2017-04-29 19:51:09 +0800

    修改auth中间件

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -4,6 +4,7 @@ const cookie = config.cookie;
 const adminNames = config.admin.names;
 const Message = require('../service').Message;
 const User = require('../service').User;
+const UserModel = require('mongoose').model('User');
 const separator = '$$$$';
 
 module.exports = {
@@ -15,19 +16,22 @@ module.exports = {
       ctx.body = '您已被管理员屏蔽了。有疑问请联系 @alsotang。';
       return;
     }
-    
+
     return next();
   },
-  authUser: async (ctx, next) => {
-    let user = ctx.session.user,
-      auth_token = ctx.cookies.get(cookie.name, {signed: true});
-      
+  authUser: async(ctx, next) => {
+    let user = ctx.session.user;
+    let auth_token = ctx.cookies.get(cookie.name, {
+      signed: true
+    });
+    
     if (!user && auth_token) {
       let user_id = auth_token.split(separator)[0];
-      user = await User.getById(user_id);
+      user = ctx.session.user = await User.getById(user_id);
     }
-
+    
     if (user) {
+      user = new UserModel(user);
       if (adminNames.indexOf(user.loginname) >= 0) {
         user.is_admin = true;
       }
@@ -41,5 +45,9 @@ module.exports = {
   gen_session: (user, ctx) => {
     let auth_token = user._id + separator; // 以后可能会存储更多信息，用 $$$$ 来分隔
     ctx.cookies.set(cookie.name, auth_token, cookie); //cookie 有效期30天
+  },
+  des_session: (ctx) => {
+    ctx.cookies.set(cookie.name, { path: cookie.path });
+    ctx.session = undefined;
   }
 };
