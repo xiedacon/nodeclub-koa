1254bd8dea011906045dc9a28a7c7ae51c85da28
xiedacon <xiedacon@qq.com>
2017-05-01 22:26:57 +0800

    update schema

--- app/schema/helper.js
+++ app/schema/helper.js
@@ -1,12 +1,60 @@
 'use strict'
+const moment = require('moment');
+const cache = require('../middleware/cache.js');
+
+const SEPARATOR = '^_^@T_T';
 
 module.exports = {
-  userRequired: (ctx, next) => {
+  userRequired: (ctx) => {
     if (!ctx.session || !ctx.session.user || !ctx.session.user._id) {
       ctx.status = 403;
       ctx.body = 'forbidden!'
       return false;
     }
-    return next();
-  }
+    return true;
+  },
+  peruserperday: makePerDayLimiter('peruserperday', function(ctx) {
+    return ctx.session.user.loginname;
+  }),
+  peripperday: makePerDayLimiter('peripperday', function(ctx) {
+    let realIP = ctx.request.get('x-real-ip');
+    if (!realIP) throw new Error('should provice `x-real-ip` header')
+
+    return realIP;
+  })
 };
+
+function makePerDayLimiter(identityName, identityFn) {
+  /*
+  options.showJson = true 表示调用来自API并返回结构化数据；否则表示调用来自前段并渲染错误页面
+  */
+  return async(ctx, name, limitCount, options) => {
+    let identity = identityFn(ctx);
+    let YYYYMMDD = moment().format('YYYYMMDD');
+    let key = YYYYMMDD + SEPARATOR + identityName + SEPARATOR + name + SEPARATOR + identity;
+
+    let count = await cache.get(key) || 0;
+
+    if (count < limitCount) {
+      count++;
+      await cache.set(key, count, 60 * 60 * 24);
+      ctx.response.set('X-RateLimit-Limit', limitCount);
+      ctx.response.set('X-RateLimit-Remaining', limitCount - count);
+      return true;
+    }else{
+      if(options.showJson){
+        ctx.body = JSON.stringify({
+          success: false,
+          error_msg: `频率限制：当前操作每天可以进行 ${limitCount} 次`
+        });
+      }else{
+        ctx.render('notify/notify', {
+          error: `频率限制：当前操作每天可以进行 ${limitCount} 次`
+        });
+      }
+      ctx.status = 403;
+      return false;
+    }
+  }
+}
+
--- app/schema/topic.js
+++ app/schema/topic.js
@@ -1,9 +1,44 @@
 'use strict'
 const helper = require('./helper.js');
+const validator = require('validator');
+
+const config = require('config-lite');
+const tabs = config.site.tabs;
 
 module.exports = {
-  create: helper.userRequired,
-  put: () => {
-    
+  create: (ctx, next) => {
+    if (!helper.userRequired(ctx)) return;
+    return next();
+  },
+  put: async (ctx, next) => {
+    if (!helper.userRequired(ctx) || !(await helper.peruserperday(ctx, 'create_topic', config.create_post_per_day, {
+        showJson: false
+      }))) return;
+
+    let title = validator.trim(ctx.request.body.title);
+    let tab = validator.trim(ctx.request.body.tab);
+    let content = validator.trim(ctx.request.body.t_content);
+    let error =
+      (title === '' && '标题不能是空的。') ||
+      ((title.length < 5 || title.length > 100) && '标题字数太多或太少。') ||
+      ((!tab || tabs.find((pair) => { pair[0] === tab })) && '必须选择一个版块。') ||
+      ((content === '') && '内容不可为空');
+
+    if (error) {
+      ctx.status = 422;
+      return ctx.render('topic/edit', {
+        edit_error: error,
+        title: title,
+        content: content
+      });
+    }
+
+    Object.assign(ctx.query, {
+      title: title,
+      tab: tab,
+      content: content
+    });
+
+    return next();
   }
 }
