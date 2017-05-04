5e6e829e8cb61e678aef15a1bdfb3d35a059537f
xiedacon <xiedacon@qq.com>
2017-05-04 21:34:55 +0800

    feat(schema): addition schema for topic.showEdit and topic.update

--- app/schema/topic.js
+++ app/schema/topic.js
@@ -1,6 +1,7 @@
 'use strict'
 const helper = require('./helper.js')
 const validator = require('validator')
+const { Topic } = require('../service')
 
 const config = require('config-lite')
 const tabs = config.site.tabs
@@ -11,22 +12,19 @@ module.exports = {
     return next()
   },
   put: async (ctx, next) => {
-    if (!helper.userRequired(ctx) || !(await helper.peruserperday(ctx, 'create_topic', config.create_post_per_day, {
-      showJson: false
-    }))) return
+    if (!helper.userRequired(ctx) ||
+      !(await helper.peruserperday(ctx, 'create_topic', config.create_post_per_day, { showJson: false }))) return
 
     let title = validator.trim(ctx.request.body.title)
     let tab = validator.trim(ctx.request.body.tab)
     let content = validator.trim(ctx.request.body.t_content)
-    let error =
-      (title === '' && '标题不能是空的。') ||
-      ((title.length < 5 || title.length > 100) && '标题字数太多或太少。') ||
-      ((!tab || tabs.find((pair) => { return tab === pair[0] }) < 0) && '必须选择一个版块。') ||
-      ((content === '') && '内容不可为空')
+    let error = checkTopicFrom(title, tab, content)
+
     if (error) {
       return ctx.renderError({
         edit_error: error,
         title: title,
+        tab: tab,
         content: content
       }, 422, 'topic/edit')
     }
@@ -45,5 +43,61 @@ module.exports = {
     if (topicId.length !== 24) return ctx.renderError('此话题不存在或已被删除。', 404)
 
     return next()
+  },
+  showEdit: async (ctx, next) => {
+    if (!helper.userRequired(ctx)) return
+
+    let topicId = ctx.params.tid
+
+    let topic = await Topic.getById(topicId)
+
+    if (!topic) return ctx.renderError('此话题不存在或已被删除。')
+
+    if (!topic.author_id.equals(ctx.session.user._id) && !ctx.session.user.is_admin) {
+      return ctx.renderError('对不起，你不能编辑此话题。', 403)
+    }
+
+    Object.assign(ctx.query, { topic: topic })
+    return next()
+  },
+  update: async (ctx, next) => {
+    if (!helper.userRequired(ctx)) return
+
+    let topicId = ctx.params.tid
+    let title = validator.trim(ctx.request.body.title)
+    let tab = validator.trim(ctx.request.body.tab)
+    let content = validator.trim(ctx.request.body.t_content)
+
+    let topic = await Topic.getById(topicId)
+
+    if (!topic) return ctx.renderError('此话题不存在或已被删除。')
+
+    if (!topic.author_id.equals(ctx.session.user._id) && !ctx.session.user.is_admin) return ctx.renderError('对不起，你不能编辑此话题。', 403)
+
+    let error = checkTopicFrom(title, tab, content)
+
+    if (error) {
+      return ctx.renderError({
+        edit_error: error,
+        title: title,
+        tab: tab,
+        content: content
+      }, 422, 'topic/edit')
+    }
+
+    topic.title = title
+    topic.tab = tab
+    topic.content = content
+
+    Object.assign(ctx.query, { topic: topic })
+
+    return next()
   }
+}
+
+function checkTopicFrom (title, tab, content) {
+  return ((title === '') && '标题不能是空的。') ||
+    ((title.length < 5 || title.length > 100) && '标题字数太多或太少。') ||
+    ((!tab || tabs.find((pair) => { return tab === pair[0] }) < 0) && '必须选择一个版块。') ||
+    ((content === '') && '内容不可为空')
 }
