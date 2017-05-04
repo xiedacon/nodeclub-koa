6d8b97c8431b137ae28aca8619c06d132bcd524e
xiedacon <xiedacon@qq.com>
2017-05-04 21:36:32 +0800

    feat(controller/reply): addition reply.add

--- app/controller/reply.js
+++ app/controller/reply.js
@@ -1,9 +1,52 @@
 'use strict'
 
+const { User, Topic, Reply } = require('../service')
+const message = require('../common/message.js')
+const at = require('../common/at.js')
+
 module.exports = {
-  add: () => {},
-  showEdit: () => {},
-  update: () => {},
-  delete: () => {},
-  up: () => {}
+  /**
+ * 添加回复
+ */
+  add: (ctx) => {
+    let topic = ctx.query.topic
+    let content = ctx.query.content
+    let replyId = ctx.query.replyId
+    let replyAuthor = ctx.session.user
+
+    return Promise.join(
+      Reply.newAndSave(content, topic._id, replyAuthor._id, replyId),
+      User.getById(topic.author_id),
+      (reply, topicAuthor) => {
+        replyAuthor.score += 5
+        replyAuthor.reply_count += 1
+
+        // 发送at消息，并防止重复 at 作者
+        at.sendMessageToMentionUsers(
+          content.replace(`@${topicAuthor.loginname}`, ''),
+          topic._id,
+          ctx.session.user._id,
+          reply._id
+        )
+
+        return Promise.all([
+          User.update(
+            { _id: replyAuthor._id },
+            { score: replyAuthor.score, reply_count: replyAuthor.reply_count }
+          ),
+          Topic.updateLastReply(topic._id, reply._id).then(() => {
+            if (!topic.author_id.equals(replyAuthor._id)) {
+              message.sendReplyMessage(topic.author_id, replyAuthor._id, topic._id, reply._id)
+            }
+          })
+        ]).then(() => {
+          ctx.redirect(`/topic/${topic._id}#${reply._id}`)
+        })
+      }
+    )
+  },
+  showEdit: () => { },
+  update: () => { },
+  delete: () => { },
+  up: () => { }
 }
