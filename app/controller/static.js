a17b852995815b908d55bc17d66d000abaf9941e
xiedacon <xiedacon@qq.com>
2017-05-08 15:34:30 +0800

    feat: addition staticController.*

--- app/controller/static.js
+++ app/controller/static.js
@@ -1,9 +1,26 @@
 'use strict'
 
 module.exports = {
-  about: () => { },
-  faq: () => { },
-  getstart: () => { },
-  robots: () => { },
-  api: () => { }
+  about: (ctx) => {
+    return ctx.render('static/about', { pageTitle: '关于我们' })
+  },
+  faq: (ctx) => {
+    return ctx.render('static/faq')
+  },
+  getstart: (ctx) => {
+    return ctx.render('static/getstart', { pageTitle: 'Node.js 新手入门' })
+  },
+  robots: (ctx) => {
+    ctx.response.type = 'text/plain'
+    return ctx.send(
+      `# See http://www.robotstxt.org/robotstxt.html for documentation on how to use the robots.txt file
+#
+# To ban all spiders from the entire site uncomment the next two lines:
+# User-Agent: *
+# Disallow: /`
+    )
+  },
+  api: (ctx) => {
+    return ctx.render('static/api')
+  }
 }
