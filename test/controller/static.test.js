d8114ee02e2f14f95a793d7337b9b4ce31f1bd8a
xiedacon <xiedacon@qq.com>
2017-05-11 20:51:46 +0800

    test: addition static.test

---
+++ test/controller/static.test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const { request } = require('../helper.js')
+const assert = require('power-assert')
+
+describe('test/controller/static.test.js', function () {
+  describe('GET /about app/controller/static.about', function () {
+    it('200', function () {
+      return request
+        .get('/about')
+        .expect(200)
+        .expect((res) => {
+          assert(res.text.includes('CNode 社区由一批热爱 Node.js 技术的工程师发起'))
+        })
+    })
+  })
+
+  describe('GET /faq app/controller/static.faq', function () {
+    it('200', function () {
+      return request
+        .get('/faq')
+        .expect(200)
+        .expect((res) => {
+          assert(res.text.includes('CNode 社区和 Node Club 是什么关系？'))
+        })
+    })
+  })
+
+  describe('GET /getstart app/controller/sttatic.getstart', function () {
+    it('200', function () {
+      return request
+        .get('/getstart')
+        .expect(200)
+        .expect((res) => {
+          assert(res.text.includes('Node.js 新手入门'))
+        })
+    })
+  })
+
+  describe('GET /robots.txt app/controller/static.robots', function () {
+    it('200', function () {
+      return request
+        .get('/robots.txt')
+        .expect(200)
+        .expect((res) => {
+          assert(res.text.includes('User-Agent: *'))
+        })
+    })
+  })
+
+  describe('GET /api app/controller/static.api', function () {
+    it('200', function () {
+      return request
+        .get('/api')
+        .expect(200)
+    })
+  })
+})
