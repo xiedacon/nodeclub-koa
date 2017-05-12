a68a4b2bd318bc0c6c664286ecc5480952b4db24
xiedacon <xiedacon@qq.com>
2017-05-12 20:24:36 +0800

    test(sign): addition test for /signup and /signin

---
+++ test/controller/sign.test.js
@@ -0,0 +1,253 @@
+'use strict'
+
+const { request, helper } = require('../support.js')
+const assert = require('power-assert')
+const Promise = require('bluebird')
+
+describe('test/controller/sign.test.js', async function () {
+  let t = Date.now()
+  let key = (i = 0) => { return `${i}YYYYtest${t}` }
+
+  let { user, user_reigsted, user_actived } = await Promise.props({
+    user: {
+      loginname: key(1),
+      email: `${key(1)}@qq.com`,
+      pass: 'test',
+      re_pass: 'test'
+    },
+    user_reigsted: helper.createUser({
+      loginname: key(2),
+      email: `${key(2)}@qq.com`,
+      pass: 'test'
+    }),
+    user_actived: helper.createUser({
+      loginname: key(3),
+      email: `${key(3)}@qq.com`,
+      pass: 'test',
+      active: true
+    })
+  })
+
+  describe('GET /signup', function () {
+    it('200', function () {
+      return request
+        .get('/signup')
+        .expect(200)
+        .expect((res) => {
+          assert(helper.includes(res.text, '确认密码'))
+        })
+    })
+  })
+
+  describe('POST /signup', function () {
+    it('200: signup new user', function () {
+      return request
+        .post('/signup')
+        .send(user)
+        .expect(200)
+        .expect((res) => {
+          assert(helper.includes(res.text, '欢迎加入 Nodeclub！我们已给您的注册邮箱发送了一封邮件，请点击里面的链接来激活您的帐号。'))
+        })
+    })
+
+    it('422: loginname exist', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { email: `test${user.email}` }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名或邮箱已被使用。'))
+        })
+    })
+
+    it('422: email exist', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { loginname: `test${user.loginname}` }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名或邮箱已被使用。'))
+        })
+    })
+
+    it('422: without loginname', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { loginname: undefined }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '信息不完整。'))
+        })
+    })
+
+    it('422: loginname.length < 5', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { loginname: 'test' }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名至少需要5个字符。'))
+        })
+    })
+
+    it('422: loginname is wrongful', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { loginname: `@${user.loginname}` }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名不合法。'))
+        })
+    })
+
+    it('422: email is wrongful', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { email: `@${user.email}` }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '邮箱不合法。'))
+        })
+    })
+
+    it('422: pass !== re_pass', function () {
+      return request
+        .post('/signup')
+        .send(Object.assign({}, user, { re_pass: 't' }))
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '两次密码输入不一致。'))
+        })
+    })
+  })
+
+  describe('POST /signout', function () {
+  })
+
+  describe('GET /signin', function () {
+    it('200', function () {
+      return request
+        .get('/signin')
+        .expect(200)
+        .expect((res) => {
+          assert(helper.includes(res.text, '登录', '通过 GitHub 登录', '忘记密码了?'))
+        })
+    })
+  })
+
+  describe('POST /signin', function () {
+    it('302: success', function () {
+      return request
+        .post('/signin')
+        .send({
+          name: user_actived.loginname,
+          pass: user_actived.pass
+        })
+        .expect(302)
+        .expect((res) => {
+          assert(helper.includes(res.headers.location, '/'))
+        })
+    })
+
+    it('302: success, loginname is email', function () {
+      return request
+        .post('/signin')
+        .send({
+          name: user_actived.email,
+          pass: user_actived.pass
+        })
+        .expect(302)
+        .expect((res) => {
+          assert(helper.includes(res.headers.location, '/'))
+        })
+    })
+
+    it('422: without loginname', function () {
+      return request
+        .post('/signin')
+        .send({
+          pass: user_reigsted.pass
+        })
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '信息不完整。'))
+        })
+    })
+
+    it('422: loginname.length < 5', function () {
+      return request
+        .post('/signin')
+        .send({
+          name: `test`,
+          pass: user_reigsted.pass
+        })
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名至少需要5个字符。'))
+        })
+    })
+
+    it('422: loginname is wrongful', function () {
+      return request
+        .post('/signin')
+        .send({
+          name: `@${user_reigsted.loginname}`,
+          pass: user_reigsted.pass
+        })
+        .expect(422)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名不合法。'))
+        })
+    })
+
+    it('403: pass is wrong', function () {
+      return request
+        .post('/signin')
+        .send({
+          name: user_reigsted.loginname,
+          pass: user_reigsted.pass + '1'
+        })
+        .expect(403)
+        .expect((res) => {
+          assert(helper.includes(res.text, '用户名或密码错误'))
+        })
+    })
+
+    it('403: user is not active', function () {
+      return request
+        .post('/signin')
+        .send({
+          name: user_reigsted.loginname,
+          pass: user_reigsted.pass
+        })
+        .expect(403)
+        .expect((res) => {
+          assert(helper.includes(res.text, `此帐号还没有被激活，激活链接已发送到 ${user_reigsted.email} 邮箱，请查收。`))
+        })
+    })
+  })
+
+  describe('GET /active_account', function () {
+
+  })
+
+  describe('GET /search_pass', function () {
+    it('200', function () {
+      return request
+        .get('/search_pass')
+        .expect(200)
+    })
+  })
+
+  describe('POST /search_pass', function () {
+
+  })
+
+  describe('GET /reset_pass', function () {
+
+  })
+
+  describe('POST /reset_pass', function () {
+
+  })
+})
