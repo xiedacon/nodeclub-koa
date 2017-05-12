6a6a91072854982046e1d0e08c21a9e1a4b8656b
xiedacon <xiedacon@qq.com>
2017-05-12 20:22:55 +0800

    test(helper): addition helper.createUser

--- test/helper.js
+++ test/helper.js
@@ -1,9 +1,29 @@
 'use strict'
 
+const { User } = require('../app/service')
+const tools = require('../app/common/tools.js')
+
 module.exports = {
   includes: (str, ...parts) => {
     if (!parts) return
 
     return parts.reduce((result, part) => { if (result) return str.includes(part) }, true)
+  },
+  createUser: async (doc) => {
+    let user = await User.newAndSave({
+      name: doc.loginname.toLowerCase(),
+      loginname: doc.loginname.toLowerCase(),
+      pass: await tools.bhash(doc.pass),
+      email: doc.email.toLowerCase(),
+      avatar_url: tools.makeGravatar(doc.email),
+      active: doc.active
+    })
+
+    user = user.toObject({ virtual: true })
+    user.pass = doc.pass
+    return user
+  },
+  getActiveUser: () => {
+
   }
 }
