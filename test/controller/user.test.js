18f0223ba0a3be35af1c85785e25e52491d16e0f
xiedacon <xiedacon@qq.com>
2017-05-14 16:37:14 +0800

    test: addition user.test.js

---
+++ test/controller/user.test.js
@@ -0,0 +1,51 @@
+'use strict'
+
+describe('test/controller/user.test.js', function () {
+  describe('GET /user/:name', function () {
+
+  })
+
+  describe('GET /user/:name/topics', function () {
+
+  })
+
+  describe('GET /user/:name/replies', function () {
+
+  })
+
+  describe('GET /setting', function () {
+
+  })
+
+  describe('POST /setting', function () {
+
+  })
+
+  describe('GET /stars', function () {
+
+  })
+
+  describe('GET /users/top100', function () {
+
+  })
+
+  describe('GET /user/:name/collections', function () {
+
+  })
+
+  describe('POST /user/set_star', function () {
+
+  })
+
+  describe('POST /user/cancel_star', function () {
+
+  })
+
+  describe('POST /user/:name/block', function () {
+
+  })
+
+  describe('POST /user/:name/delete_all', function () {
+
+  })
+})
