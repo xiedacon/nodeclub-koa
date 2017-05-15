b6dc9dd37d63473800511a4e3af0f06f32d8c54e
xiedacon <xiedacon@qq.com>
2017-05-15 15:17:14 +0800

    test(user.test.js): addition test to GET /setting

--- test/controller/user.test.js
+++ test/controller/user.test.js
@@ -88,7 +88,59 @@ describe('test/controller/user.test.js', function () {
   })
 
   describe('GET /setting', function () {
+    let cookies
+    before(function () {
+      return request
+        .post('/signin')
+        .send({
+          name: user.loginname,
+          pass: user.pass
+        })
+        .expect((res) => {
+          cookies = res.headers['set-cookie'].reduce((str, cookie) => {
+            return str + cookie.split(';')[0] + ';'
+          }, '')
+        })
+    })
 
+    it('200: success', function () {
+      return request
+        .get('/setting')
+        .set('Cookie', cookies)
+        .expect(200)
+        .expect((res) => {
+          assert(helper.includes(res.text, [
+            user.name,
+            user.email,
+            '保存设置',
+            'Access Token',
+            '积分: 0'
+          ]))
+        })
+    })
+
+    it('200: success with message', function () {
+      return request
+        .get('/setting')
+        .query({ save: 'success' })
+        .set('Cookie', cookies)
+        .expect(200)
+        .expect((res) => {
+          assert(helper.includes(res.text, [
+            user.name,
+            user.email,
+            '保存设置',
+            'Access Token',
+            '保存成功。'
+          ]))
+        })
+    })
+
+    it('403: without cookie', function () {
+      return request
+        .get('/setting')
+        .expect(403)
+    })
   })
 
   describe('POST /setting', function () {
