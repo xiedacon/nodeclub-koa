a72880fb1e32d8434c41605f4805667d324b4d69
xiedacon <xiedacon@qq.com>
2017-05-30 09:11:48 +0800

    test(helper.test.js): addition test to helper.makePerDayLimiter

--- test/schema/helper.test.js
+++ test/schema/helper.test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const schema = require('../../app/schema/helper.js')
-const { helper } = require('../support.js')
+const { helper, uuid } = require('../support.js')
 const assert = require('power-assert')
 const rewire = require('rewire')
 
@@ -44,6 +44,31 @@ describe('test/schema/helper.test.js', function () {
   })
 
   describe('#makePerDayLimiter', function () {
+    let _schema = rewire('../../app/schema/helper.js').__get__('makePerDayLimiter')('test', () => { return 'test' })
+    let ctx = {
+      response: { set: () => { } }
+    }
 
+    it('should return true when count < limitCount', async function () {
+      assert(await _schema(ctx, uuid(), 5))
+    })
+
+    it('should return false and call ctx.send when count < limitCount and options.showJson is given', async function () {
+      ctx.send = (msg, code) => {
+        assert(msg === JSON.stringify({ success: false, error_msg: `频率限制：当前操作每天可以进行 -1 次` }))
+        assert(code === 403)
+      }
+      ctx.renderError = () => { throw new Error('should not call this') }
+      assert(!await _schema(ctx, uuid(), -1, { showJson: true }))
+    })
+
+    it('should return false and call ctx.renderError when count < limitCount but options.showJson is not given', async function () {
+      ctx.send = () => { throw new Error('should not call this') }
+      ctx.renderError = (msg, code) => {
+        assert(msg === '频率限制：当前操作每天可以进行 -1 次')
+        assert(code === 403)
+      }
+      assert(!await _schema(ctx, uuid(), -1, {}))
+    })
   })
 })
